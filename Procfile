91da93a3d1c20669a77aefc84ded51b97d7dde16
Godwinswinton <106500866+Godwinswinton@users.noreply.github.com>
2022-07-09 22:56:47 +0530

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn model:app --log-level debug
---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, render_template
+import pandas as pd
+import pickle
+
+app = Flask(__name__)
+data=pd.read_csv('house price.csv')
+pipe=pickle.load(open("model.pkl", 'rb'))
+
+@app.route('/')
+def index():
+    locations= sorted(data['location'].unique())
+    return render_template('app.html',locations=locations)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    bhk = int(request.form['bhk'])
+    bath = int(request.form['bath'])
+
+    print(location, bhk, bath, total_sqft)
+    input=pd.DataFrame([[location,total_sqft,bath,bhk]], columns=['location','total_sqft','bath','bhk'])
+    prediction=pipe.predict(input)[0] *1e5
+
+    return str(prediction)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
